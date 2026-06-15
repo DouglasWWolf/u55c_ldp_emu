f8d23d39dea651d6a4ee91219b33f88df640b5f1
DouglasWWolf <douglaswolf@yahoo.com>
2026-06-15 10:25:22 -0700

    No commit message

--- project.gen/sources_1/bd/mref/ldp_manager/xgui/ldp_manager_v1_0.tcl
+++ project.gen/sources_1/bd/mref/ldp_manager/xgui/ldp_manager_v1_0.tcl
@@ -5,7 +5,6 @@ proc init_gui { IPINST } {
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
   ipgui::add_param $IPINST -name "AW" -parent ${Page_0}
   ipgui::add_param $IPINST -name "DW" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "FRAME_DROP_ERR_BIT" -parent ${Page_0}
 
 
 }
@@ -28,15 +27,6 @@ proc validate_PARAM_VALUE.DW { PARAM_VALUE.DW } {
 	return true
 }
 
-proc update_PARAM_VALUE.FRAME_DROP_ERR_BIT { PARAM_VALUE.FRAME_DROP_ERR_BIT } {
-	# Procedure called to update FRAME_DROP_ERR_BIT when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.FRAME_DROP_ERR_BIT { PARAM_VALUE.FRAME_DROP_ERR_BIT } {
-	# Procedure called to validate FRAME_DROP_ERR_BIT
-	return true
-}
-
 
 proc update_MODELPARAM_VALUE.DW { MODELPARAM_VALUE.DW PARAM_VALUE.DW } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -46,10 +36,5 @@ proc update_MODELPARAM_VALUE.DW { MODELPARAM_VALUE.DW PARAM_VALUE.DW } {
 proc update_MODELPARAM_VALUE.AW { MODELPARAM_VALUE.AW PARAM_VALUE.AW } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.AW}] ${MODELPARAM_VALUE.AW}
-}
-
-proc update_MODELPARAM_VALUE.FRAME_DROP_ERR_BIT { MODELPARAM_VALUE.FRAME_DROP_ERR_BIT PARAM_VALUE.FRAME_DROP_ERR_BIT } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.FRAME_DROP_ERR_BIT}] ${MODELPARAM_VALUE.FRAME_DROP_ERR_BIT}
 }
 
