50fda47a71f364d2df91e773b041fdebc6801422
DouglasWWolf <douglaswolf@yahoo.com>
2026-06-30 11:12:15 -0700

    No commit message

--- project.gen/sources_1/bd/mref/ldp_buffer/xgui/ldp_buffer_v1_0.tcl
+++ project.gen/sources_1/bd/mref/ldp_buffer/xgui/ldp_buffer_v1_0.tcl
@@ -4,6 +4,8 @@ proc init_gui { IPINST } {
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
   ipgui::add_param $IPINST -name "DATA_FIFO_DEPTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA_FIFO_RAM_TYPE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DATA_FIFO_SECTIONS" -parent ${Page_0}
   ipgui::add_param $IPINST -name "DW" -parent ${Page_0}
   ipgui::add_param $IPINST -name "SUPPORT_FIFO_DEPTH" -parent ${Page_0}
 
@@ -16,6 +18,24 @@ proc update_PARAM_VALUE.DATA_FIFO_DEPTH { PARAM_VALUE.DATA_FIFO_DEPTH } {
 
 proc validate_PARAM_VALUE.DATA_FIFO_DEPTH { PARAM_VALUE.DATA_FIFO_DEPTH } {
 	# Procedure called to validate DATA_FIFO_DEPTH
+	return true
+}
+
+proc update_PARAM_VALUE.DATA_FIFO_RAM_TYPE { PARAM_VALUE.DATA_FIFO_RAM_TYPE } {
+	# Procedure called to update DATA_FIFO_RAM_TYPE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_FIFO_RAM_TYPE { PARAM_VALUE.DATA_FIFO_RAM_TYPE } {
+	# Procedure called to validate DATA_FIFO_RAM_TYPE
+	return true
+}
+
+proc update_PARAM_VALUE.DATA_FIFO_SECTIONS { PARAM_VALUE.DATA_FIFO_SECTIONS } {
+	# Procedure called to update DATA_FIFO_SECTIONS when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_FIFO_SECTIONS { PARAM_VALUE.DATA_FIFO_SECTIONS } {
+	# Procedure called to validate DATA_FIFO_SECTIONS
 	return true
 }
 
@@ -46,6 +66,16 @@ proc update_MODELPARAM_VALUE.DW { MODELPARAM_VALUE.DW PARAM_VALUE.DW } {
 proc update_MODELPARAM_VALUE.DATA_FIFO_DEPTH { MODELPARAM_VALUE.DATA_FIFO_DEPTH PARAM_VALUE.DATA_FIFO_DEPTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.DATA_FIFO_DEPTH}] ${MODELPARAM_VALUE.DATA_FIFO_DEPTH}
+}
+
+proc update_MODELPARAM_VALUE.DATA_FIFO_SECTIONS { MODELPARAM_VALUE.DATA_FIFO_SECTIONS PARAM_VALUE.DATA_FIFO_SECTIONS } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_FIFO_SECTIONS}] ${MODELPARAM_VALUE.DATA_FIFO_SECTIONS}
+}
+
+proc update_MODELPARAM_VALUE.DATA_FIFO_RAM_TYPE { MODELPARAM_VALUE.DATA_FIFO_RAM_TYPE PARAM_VALUE.DATA_FIFO_RAM_TYPE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_FIFO_RAM_TYPE}] ${MODELPARAM_VALUE.DATA_FIFO_RAM_TYPE}
 }
 
 proc update_MODELPARAM_VALUE.SUPPORT_FIFO_DEPTH { MODELPARAM_VALUE.SUPPORT_FIFO_DEPTH PARAM_VALUE.SUPPORT_FIFO_DEPTH } {
